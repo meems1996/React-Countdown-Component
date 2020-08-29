038c80c64df90315fc4a4badef799f5ea547f589
Maria Shimkovska <mshimkov@stetson.edu>
2020-08-29 09:55:16 -0700

    readme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CountdownContainer from './components/CountdownContainer';
 
 ReactDOM.render(
   <div>
-    <CountdownContainer targetDate="August 27 2020 23:36:00" inactiveMessage="Countdown over" />
+    <CountdownContainer targetDate="August 30 2020 23:36:00" inactiveMessage="Countdown over" />
   </div>,
   document.getElementById('root')
 );
